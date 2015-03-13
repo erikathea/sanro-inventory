a0b83ec0da3a1009048217022eaf57a26a0652bb
erikathea <ethajes@gmail.com>
2015-03-13 16:40:02 +0800

    added validation for incoming receipts - combination of supplier & receipt_number

--- app/models/incoming_receipt.rb
+++ app/models/incoming_receipt.rb
@@ -3,6 +3,7 @@ class IncomingReceipt < ActiveRecord::Base
   accepts_nested_attributes_for :incoming_receipt_details, :reject_if => :all_blank, :allow_destroy => true
 
   validate :has_one_receipt_detail?
+  validate :check_supplier_receipt_number?
   validates :date_issued, presence: :true
   validates :receipt_number, presence: :true
   validates :supplier, presence: :true
@@ -14,6 +15,12 @@ class IncomingReceipt < ActiveRecord::Base
   def has_one_receipt_detail?
     if self.incoming_receipt_details.empty?
       errors.add(:incoming_receipt_details, ': Please add at least one receipt item')
+    end
+  end
+
+  def check_supplier_receipt_number?
+    if(Inventory.find_by_receipt_number_and_supplier(self.receipt_number, self.supplier))
+      errors.add(:receipt_number, 'already exists')
     end
   end
 
