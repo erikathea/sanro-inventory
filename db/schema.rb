801b7f76877269cb8b2e474d3f0b753143067d8d
erikathea <ethajes@gmail.com>
2015-04-01 00:25:00 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327231848) do
+ActiveRecord::Schema.define(version: 20150331161706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150327231848) do
     t.decimal  "selling_price", precision: 15, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remarks"
   end
 
   create_table "outgoing_receipt_details", force: true do |t|
@@ -92,6 +93,7 @@ ActiveRecord::Schema.define(version: 20150327231848) do
     t.integer  "sale_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "po_no"
   end
 
   create_table "users", force: true do |t|
